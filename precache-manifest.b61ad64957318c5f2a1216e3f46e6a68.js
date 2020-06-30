7652849442d9f57ad1245be3258a2fcc07a9da50
hachitubg <tuxidenbg123@gmail.com>
2020-06-30 14:23:21 +0700

    Updates

---
+++ precache-manifest.b61ad64957318c5f2a1216e3f46e6a68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9d68e7234578abe8d37ea2f1e210c3",
+    "url": "/cvhachitu/index.html"
+  },
+  {
+    "revision": "e8e49c0b2fb8c1846582",
+    "url": "/cvhachitu/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "512bd74bdb3956020b0b",
+    "url": "/cvhachitu/static/js/2.48d8b463.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cvhachitu/static/js/2.48d8b463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e49c0b2fb8c1846582",
+    "url": "/cvhachitu/static/js/main.74677e7c.chunk.js"
+  },
+  {
+    "revision": "d344573bfeb01436358f",
+    "url": "/cvhachitu/static/js/runtime-main.7d42517b.js"
+  },
+  {
+    "revision": "d31f5f21253ab2beeda9987eba49780e",
+    "url": "/cvhachitu/static/media/avatar.d31f5f21.jpg"
+  }
+]);
